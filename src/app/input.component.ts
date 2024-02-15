c352d657d86858a409b1f6f4c70b0635ae43f819
Younes Jaaidi <younes@marmicode.io>
2024-02-15 21:41:06 +0100

    feat: ✨ host binding / listerner alternatives

--- src/app/input.component.ts
+++ src/app/input.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, ElementRef, effect, model, signal, viewChild } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { ChangeDetectionStrategy, Component, DestroyRef, ElementRef, effect, inject, model, signal, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -18,18 +19,29 @@ import { FormsModule } from '@angular/forms';
 })
 export class InputComponent {
   value = model.required<string>();
+
   inputEl = viewChild('input', {
     read: ElementRef
   });
   isInputDisplayed = signal(true);
 
   constructor() {
-    effect(() => {
-      console.log(this.inputEl()?.nativeElement.outerHTML);
-    });
+    addDocumentListener('click', () => console.log('click'));
+    setClass('my-class');
   }
 
   toggle() {
     this.isInputDisplayed.update(displayed => !displayed);
   }
+}
+
+function setClass(className: string) {
+    inject(ElementRef).nativeElement.classList.add(className);
+}
+
+function addDocumentListener(eventName: 'click', cb: () => void) {
+  const document = inject(DOCUMENT);
+  const destroyRef = inject(DestroyRef);
+  document.addEventListener(eventName, cb);
+  destroyRef.onDestroy(() => document.removeEventListener(eventName, cb));
 }
