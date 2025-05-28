51c8c2cda148797e20e5bbb64abe190d3e1798c4
Younes Jaaidi <younes@marmicode.io>
2025-05-28 17:37:13 +0200

    feat: live search

--- src/app/recipe-filter.ng.ts
+++ src/app/recipe-filter.ng.ts
@@ -1,11 +1,13 @@
 import { Component, output } from '@angular/core';
+import { outputFromObservable } from '@angular/core/rxjs-interop';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { map } from 'rxjs';
 import { RecipeCriteria } from './recipe-criteria';
 
 @Component({
   selector: 'app-recipe-filter',
   imports: [ReactiveFormsModule],
-  template: ` <form [formGroup]="form" (ngSubmit)="triggerCriteriaChange()">
+  template: ` <form [formGroup]="form" (ngSubmit)="triggerCriteriaSubmit()">
     <input type="text" formControlName="keywords" placeholder="Keywords" />
     <input
       type="number"
@@ -18,19 +20,25 @@ import { RecipeCriteria } from './recipe-criteria';
   </form>`,
 })
 export class RecipeFilter {
-  criteriaChange = output<RecipeCriteria>();
-
   protected form = new FormGroup({
     keywords: new FormControl<string | null>(null),
     maxIngredients: new FormControl<number | null>(null),
     maxSteps: new FormControl<number | null>(null),
   });
+  criteriaChange = outputFromObservable(
+    this.form.valueChanges.pipe(map(convertToCriteria)),
+  );
+  criteriaSubmit = output<RecipeCriteria>();
 
-  protected triggerCriteriaChange() {
-    this.criteriaChange.emit({
-      keywords: this.form.value.keywords ?? null,
-      maxIngredients: this.form.value.maxIngredients ?? null,
-      maxSteps: this.form.value.maxSteps ?? null,
-    });
+  protected triggerCriteriaSubmit() {
+    this.criteriaSubmit.emit(convertToCriteria(this.form.value));
   }
+}
+
+function convertToCriteria(value: Partial<RecipeCriteria>): RecipeCriteria {
+  return {
+    keywords: value.keywords ?? null,
+    maxIngredients: value.maxIngredients ?? null,
+    maxSteps: value.maxSteps ?? null,
+  };
 }
