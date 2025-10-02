4439e8144c4c1fd74b44637f2d8e2072f995a7da
Cooker <kitchen@marmicode.io>
2025-10-02 11:23:48 +0200

    feat: ✨ focus on 402-recipe-search-filter-interaction-solution

--- apps/502-recipe-filter-ct-starter/src/app/recipe/recipe-filter.ng.pw.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@jscutlery/playwright-ct-angular';
-
-test.describe('<wm-recipe-filter>', () => {
-  test('should trigger filterChange', async ({ mount }) => {
-    test.skip(true, '🚧 work in progress');
-  });
-});
