0737f6e4ea8b54ab1fb731533cfb9806d447474c
Cooker <kitchen@marmicode.io>
2025-10-02 13:41:59 +0200

    feat: ✨ focus on 404-recipe-filter-material-starter

--- apps/502-recipe-filter-ct-starter/src/app/recipe/recipe-filter.ng.pw.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@jscutlery/playwright-ct-angular';
-
-test.describe('RecipeFilter', () => {
-  test('should trigger filterChange', async ({ mount }) => {
-    test.skip(true, '🚧 work in progress');
-  });
-});
