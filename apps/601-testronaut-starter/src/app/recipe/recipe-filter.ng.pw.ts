a895e9ff155285b80d3d98aa6b407d9adabd5966
Cooker <kitchen@marmicode.io>
2025-10-02 12:05:31 +0200

    feat: ✨ focus on 101-meal-planner-solution

--- apps/601-testronaut-starter/src/app/recipe/recipe-filter.ng.pw.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from '@testronaut/angular';
-import { RecipeFilter } from './recipe-filter.ng';
-
-test.describe('RecipeFilter', () => {
-  test('should trigger filterChange', async ({ page, mount }) => {
-    test.skip(true, '🚧 work in progress!');
-  });
-});
--- apps/601-testronaut-starter/src/app/recipe/recipe-preview.ng.pw.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@testronaut/angular';
-import { recipeMother } from '../testing/recipe.mother';
-import { RecipePreview } from './recipe-preview.ng';
-
-test.describe('RecipePreview', () => {
-  test('should show recipe name', async ({ page, mount }) => {
-    test.skip(true, '🚧 work in progress!');
-  });
-});
