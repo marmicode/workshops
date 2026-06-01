aff4d98ba04ce17a1def5fa0ce7fea7742e26753
Younes Jaaidi <younes@marmicode.io>
2026-06-01 22:04:32 +0200

    build: 📦 set up angular material

--- apps/whiskmate/src/app/app.ts
+++ apps/whiskmate/src/app/app.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, viewChild } from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import {
+  MatListItem,
+  MatListItemIcon,
+  MatListItemTitle,
+  MatNavList,
+} from '@angular/material/list';
+import {
+  MatSidenav,
+  MatSidenavContainer,
+  MatSidenavContent,
+} from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    RouterOutlet,
+    MatToolbar,
+    MatSidenavContainer,
+    MatSidenav,
+    MatSidenavContent,
+    MatIcon,
+    MatNavList,
+    MatListItem,
+    MatListItemIcon,
+    MatListItemTitle,
+  ],
   selector: 'wm-root',
-  template: `<router-outlet />`,
+  template: `
+    <mat-sidenav-container class="shell">
+      <mat-sidenav #drawer mode="side" opened>
+        <mat-nav-list>
+          <a mat-list-item href="#">
+            <mat-icon matListItemIcon>home</mat-icon>
+            <span matListItemTitle>Home</span>
+          </a>
+        </mat-nav-list>
+      </mat-sidenav>
+
+      <mat-sidenav-content>
+        <mat-toolbar>
+          <span>Whiskmate</span>
+        </mat-toolbar>
+
+        <main class="content">
+          <router-outlet />
+        </main>
+      </mat-sidenav-content>
+    </mat-sidenav-container>
+  `,
 })
-export class App {}
+export class App {
+  private readonly drawer = viewChild.required<MatSidenav>('drawer');
+
+  toggleDrawer(): void {
+    this.drawer().toggle();
+  }
+}
